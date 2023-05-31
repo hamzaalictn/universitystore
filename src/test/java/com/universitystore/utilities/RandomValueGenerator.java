67b505cbe7166789cd8c381dfc11d020af700faa
Hamza <r.hmzctn@gmail.com>
2023-05-31 12:00:09 +0200

    Adding RandomValueGenerator

--- src/test/java/com/universitystore/utilities/RandomValueGenerator.java
+++ src/test/java/com/universitystore/utilities/RandomValueGenerator.java
@@ -2,7 +2,7 @@ package com.universitystore.utilities;
 
 import java.util.Random;
 
-public class StringUtils {
+public class RandomValueGenerator {
     public static String generateRandomAlphanumericString(int length) {
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // Include more characters if you want
         StringBuilder sb = new StringBuilder(length);
@@ -15,5 +15,17 @@ public class StringUtils {
         }
 
         return sb.toString();
+    }
+
+    public static long generateRandomNumber(int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length must be a positive integer");
+        }
+
+        Random rand = new Random();
+        long minimum = (long) Math.pow(10, length - 1);
+        long maximum = (long) Math.pow(10, length) - 1;
+
+        return minimum + (long) (rand.nextDouble() * (maximum - minimum));
     }
 }
